993d32af1d4084d5ff0f28d043351f2ad509d0d4
zenzx <2362315840@qq.com>
2021-06-11 20:50:00 +0800

    添加user_email，user_passwd

--- fw-robot/data.py
+++ fw-robot/data.py
@@ -1,6 +1,15 @@
 import os
+import json
 
 print("========请为配置你的邮箱========")
 user_email = input("请输入你的网易邮箱：")
 user_passwd = input("请输入你的邮箱密码(应为SMTP秘钥)：")
 # TODO: 将数据添加到json文件->data.json中
+data_dict = {
+	'user':{
+		'user_email': user_email,
+		'user_passwd': user_passwd,
+	},
+}
+data = json.dumps(data_dict)
+#print(data)
